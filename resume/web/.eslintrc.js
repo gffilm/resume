a10facaf04f9d531cba7ccd3fbcdc80e361489db
Gershon Fiedler <gffilm@gmail.com>
2024-02-22 22:51:12 -0600

    attributes

--- resume/web/.eslintrc.js
+++ resume/web/.eslintrc.js
@@ -3,4 +3,14 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
   extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  rules: {
+   // Remove trailing spaces
+    'no-trailing-spaces': 'error',
+    // Remove trailing commas
+    'comma-dangle': ['error', 'never'],
+    // Remove semicolons
+    semi: ['error', 'never'],
+    // Adjust tabs to double spaces
+    'indent': ['error', 2, { 'SwitchCase': 1 }]
+  }
 };
--- resume/web/src/index.tsx
+++ resume/web/src/index.tsx
@@ -1,7 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
 
-const test = null;
-
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'))
