44484a684e0f4950ae7e241ac7d093028ad032f6
Gershon Fiedler <gffilm@gmail.com>
2024-02-27 22:35:38 -0600

    updates

--- tables/web/src/Seating.tsx
+++ tables/web/src/Seating.tsx
@@ -29,20 +29,16 @@ const Seating = () => {
 
   const [tables, setTables] = useState([
     { id: 1, name: 'Table 1', max: 10, people: [] },
-    { id: 2, name: 'Table 2', max: 10, people: [] },
+    { id: 2, name: 'Table 2', max: 8, people: [] },
     { id: 3, name: 'Table 3', max: 10, people: [] },
     { id: 4, name: 'Table 4', max: 10, people: [] },
     { id: 5, name: 'Table 5', max: 10, people: [] },
     { id: 6, name: 'Table 6', max: 10, people: [] },
-    { id: 7, name: 'Table 7', max: 10, people: [] },
-    { id: 8, name: 'Table 8', max: 10, people: [] },
-    { id: 9, name: 'Table 9', max: 10, people: [] },
-    { id: 10, name: 'Table 10', max: 10, people: [] },
-    { id: 11, name: 'Table 11', max: 10, people: [] },
-    { id: 12, name: 'Table 12', max: 10, people: [] },
-    { id: 13, name: 'Table 13', max: 10, people: [] },
-    { id: 14, name: 'Table 14', max: 10, people: [] },
-    { id: 15, name: 'Table 15', max: 29, people: [] },
+    { id: 8, name: 'Table 7', max: 10, people: [] },
+    { id: 9, name: 'Table 8', max: 10, people: [] },
+    { id: 10, name: 'Table 9', max: 8, people: [] },
+    { id: 12, name: 'Table 10', max: 10, people: [] },
+    { id: 15, name: 'Table 11', max: 34, people: [] },
   ])
 
   useEffect(() => {
